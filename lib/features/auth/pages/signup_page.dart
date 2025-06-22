19afcbac0bc060ed431d5b0861dcde4dc19bf476
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-22 23:42:22 +0530

    signup page

--- lib/features/auth/pages/signup_page.dart
+++ lib/features/auth/pages/signup_page.dart
@@ -40,7 +40,11 @@ class _SignupPageState extends State<SignupPage> {
               decoration: InputDecoration(
                 hintText: 'Email',
                 contentPadding: const EdgeInsets.all(27),
-                enabledBorder: OutlineInputBorder(),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    
+                  )
+                ),
               ),
             ),
 
