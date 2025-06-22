0ebcdc53e5db7b7cb00d9646a3cf21ee68e5aed6
Piyushbhatt7 <vengersmon003@gmail.com>
2025-06-22 22:39:54 +0530

    singup page

---
+++ lib/features/auth/pages/signup_page.dart
@@ -0,0 +1,13 @@
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
