c0ea83a5f31aac47cf82e0ac00fed7fc0cdb6b6b
Piyushbhatt7 <vengersmon003@gmail.com>
2025-07-02 22:18:53 +0530

    setting database

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,9 @@
 import express from "express";
+import authRouter from "./routes/auth";
 
 const app = express();
 
-app.use
+app.use("/auth",authRouter);
 
 app.get("/", (req, res) => {
     res.send("Welcome to my app");
