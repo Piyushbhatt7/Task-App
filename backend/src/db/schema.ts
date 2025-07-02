f864304f84d972dc764b1d55078965246cadbfb9
Piyushbhatt7 <vengersmon003@gmail.com>
2025-07-02 22:26:24 +0530

    setting database

--- backend/src/db/schema.ts
+++ backend/src/db/schema.ts
@@ -1,6 +1,8 @@
 import { uuid } from "drizzle-orm/gel-core";
 import { pgTable } from "drizzle-orm/pg-core";
+import { text } from "stream/consumers";
 
 pgTable("users", {
-    id: uuid("id").primaryKey()
+    id: uuid("id").primaryKey().defaultRandom(),
+    name: text("name").notNull().unique(),
 })
