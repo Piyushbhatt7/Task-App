26f8a6e7bf0b1911b289b135423c4121d98c0fd0
Piyushbhatt7 <vengersmon003@gmail.com>
2025-07-02 22:12:52 +0530

    setting database

--- backend/src/db/index.ts
+++ backend/src/db/index.ts
@@ -1,5 +1,8 @@
+import { drizzle } from "drizzle-orm/singlestore/driver";
 import { Pool } from "pg";
 
 const pool = new Pool({
     connectionString: "postgresql://postgres:piyush@123@mydb:5434/mydb",
-})
+});
+
+export const db = drizzle()
