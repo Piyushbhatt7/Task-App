d00e65f896c127b0d4b7b9ec756b881e22148dec
Piyushbhatt7 <vengersmon003@gmail.com>
2025-07-03 11:50:55 +0530

    creating schema to store user

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -5,6 +5,8 @@ const authRouter = Router();
 
 authRouter.post("/signup", async (req, res) => {
     try {
+        // get req body
+        // check if user already exists
         // create a new  user and store in db
     } catch (error) {
         res.status(500).json({error: error});
